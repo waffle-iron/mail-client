49dd2d3d44d28f7037ba893e75344aa6f0978fc6
dieptran00888 <dieptx.ptit@gmail.com>
2017-10-27 14:02:34 +0700

    Finish test encrypt and decrypt by pgp

--- src/pgp/pgp.java
+++ src/pgp/pgp.java
@@ -18,7 +18,12 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.Security;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import javax.crypto.BadPaddingException;
@@ -33,50 +38,49 @@ import javax.crypto.spec.SecretKeySpec;
  * @author HackerStrawHat
  */
 public class pgp {
-    private static final byte[] key16byteTest =
-            new byte[]{'T', 'h', 'e', 'B', 'e', 's', 't', 'S', 'e', 'c', 'r', 'e', 't', 'K', 'e', 'y'};
-    private static Key keyPrivate;
+    private static String key16byteTest = "TheBestSecretKey";
+//    private static Key keyPrivate;
+    private static final String _AES_ALGORITHM_NAME = "AES";
+    private static final String _RSA_ALGORITHM_NAME = "RSA";
 
-  public static byte[] encryptSessionKeyRSA(byte[] sessionKey, PublicKey key) throws GeneralSecurityException {
-    Cipher cipher = Cipher.getInstance("RSA");
+  public static byte[] encryptSessionKeyRSA(String sessionKey, PublicKey key) throws GeneralSecurityException {
+    Cipher cipher = Cipher.getInstance(_RSA_ALGORITHM_NAME);
     cipher.init(Cipher.ENCRYPT_MODE, key);
-//    keyPrivate = pair.getPrivate();
-    return cipher.doFinal(sessionKey);
+    return cipher.doFinal(sessionKey.getBytes());
   }
 
   public static byte[] decryptSessionKeyRSA(byte[] encryptedSessionKey, PrivateKey key) throws GeneralSecurityException {
-    Cipher cipher = Cipher.getInstance("RSA");
+    Cipher cipher = Cipher.getInstance(_RSA_ALGORITHM_NAME);
     cipher.init(Cipher.DECRYPT_MODE, key);
 
     byte[] encodedPublicKey = cipher.doFinal(encryptedSessionKey);
     return encodedPublicKey;
   }
 
-  protected static PublicKey getPublicKey(byte[] encodedKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
-    KeyFactory factory = KeyFactory.getInstance("RSA");
-    X509EncodedKeySpec encodedKeySpec = new X509EncodedKeySpec(encodedKey);
-    return factory.generatePublic(encodedKeySpec);
-  }
-
-  protected static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
-    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+  private static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(_RSA_ALGORITHM_NAME);
     keyPairGenerator.initialize(2048, SecureRandom.getInstance("SHA1PRNG"));
     return keyPairGenerator.generateKeyPair();
   }
-  
-    public static void main(String[] args) throws NoSuchAlgorithmException, GeneralSecurityException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, Exception {
+//  
+//    public static void main(String[] args) throws NoSuchAlgorithmException, GeneralSecurityException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, Exception {
+//        Test AES
 //        System.out.println(encryptMessageByAES("This is message"));
 //        System.out.println("========");
 //        System.out.println(decryptMessagaeByAES("xeJ4ARR6qM5/nNn4IOjXM8PKjBSlw+yRhjPv1wfyPS7tZ/iwACEhg99kKS2yfsAZ"));
-//        System.out.println(encryptPublicKeyRSA(keyValue));
-//        System.out.println("key private = "+ keyPrivate.toString().getBytes());
-//        System.out.println(decryptPublicKeyRSA("[B@224aed64".getBytes()));
-        KeyPair pair = generateKeyPair();
-
-        keyPrivate = pair.getPrivate();
-        System.out.println("encode" + encryptSessionKeyRSA(key16byteTest, pair.getPublic()));
-        System.out.println("decode" +decryptSessionKeyRSA(encryptSessionKeyRSA(key16byteTest, pair.getPublic()), pair.getPrivate()));
-    }
+//        
+//        Test RSA
+//        KeyPair pair = generateKeyPair();
+//        System.out.println(encryptSessionKeyRSA(key16byteTest, pair.getPublic()));
+//        System.out.println(new String(decryptSessionKeyRSA(encryptSessionKeyRSA(key16byteTest, pair.getPublic()),pair.getPrivate())));
+//    }
+  
+  //
+//  protected static PublicKey getPublicKey(byte[] encodedKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+//    KeyFactory factory = KeyFactory.getInstance("RSA");
+//    X509EncodedKeySpec encodedKeySpec = new X509EncodedKeySpec(encodedKey);
+//    return factory.generatePublic(encodedKeySpec);
+//  }
 
     // Sinh sessionKey 128bit
 //    private static Key generateSessionKey() throws NoSuchAlgorithmException {
@@ -84,13 +88,15 @@ public class pgp {
 //        keyGenerator.init(128, new SecureRandom());
 //        return keyGenerator.generateKey();
 //    }
-    public static Key generateKeyAES() throws Exception {
-        return new SecretKeySpec(key16byteTest, "AES");
+    private static Key generateKeyAES() throws Exception {
+        return new SecretKeySpec(key16byteTest.getBytes(), _AES_ALGORITHM_NAME);
     }
     
     // encrypt data by AES algorithm
-    public static String encryptMessageByAES(String data) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, Exception {
-        Cipher cipher = Cipher.getInstance("AES");
+    public static String encryptMessageByAES(String data) throws 
+            NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException, Exception {
+        Cipher cipher = Cipher.getInstance(_AES_ALGORITHM_NAME);
         System.out.println(generateKeyAES().toString().getBytes());
         cipher.init(Cipher.ENCRYPT_MODE, generateKeyAES());
         byte[] encryptValue = cipher.doFinal(data.getBytes());
@@ -98,8 +104,10 @@ public class pgp {
     }
     
     // decrypt data by AES algorithm
-    public static String decryptMessagaeByAES(String encryptedData) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, Exception {
-        Cipher c = Cipher.getInstance("AES");
+    public static String decryptMessagaeByAES(String encryptedData) throws 
+            NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException, Exception {
+        Cipher c = Cipher.getInstance(_AES_ALGORITHM_NAME);
         c.init(Cipher.DECRYPT_MODE, generateKeyAES());
         byte[] decordedValue = Base64.getDecoder().decode(encryptedData);
         byte[] decryptValue = c.doFinal(decordedValue);
