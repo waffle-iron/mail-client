ef3c8ad26b686959ef4cc3ec4ce434b693b9f8df
dieptran00888 <dieptx.ptit@gmail.com>
2017-10-30 15:14:42 +0700

    Add session key to file

---
+++ src/KeyFileIO/KeyFileIO.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KeyFileIO;
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author tran.xuan.diep
+ */
+public final class KeyFileIO {
+    public final static void writeKeyToFile(String key) {
+        FileOutputStream fos = null;
+        File file;
+        try {
+            file = new File("keyFile.txt");
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            fos = new FileOutputStream(file);
+            fos.write(key.getBytes());
+            fos.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public final static String readKeyFromFile() {
+        
+        File file = new File("keyFile.txt"); 
+        String keyResult = "";
+        try {
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNext()) {
+                keyResult = scanner.next();
+            }
+            scanner.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return keyResult;
+    }
+}
